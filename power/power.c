b71b8bb8001811ee988642e4dfc6a2c0526a7143
Schischu <schischu65@gmail.com>
2015-11-25 20:03:15 +0100

    power: Add support for double tap tp wake

--- power/power.c
+++ power/power.c
@@ -25,7 +25,14 @@
 #include <hardware/hardware.h>
 #include <hardware/power.h>
 
-#define TSP_POWER "/sys/class/input/input8/enabled"
+//sec_touchscreen
+#define TSP_POWER        "/sys/class/input/input1/enabled"
+#define TSP_WAKE_GESTURE "/sys/class/input/input1/wake_gesture"
+
+#define GKP_POWER        "/sys/class/input/input8/enabled"
+#define TKP_POWER        "/sys/class/input/input9/enabled"
+
+static unsigned char wake_gesture_enabled = 0;
 
 static int write_int(char const *path, int value)
 {
@@ -59,15 +66,57 @@ static void power_init(struct power_module *module)
 
 static void power_set_interactive(struct power_module *module, int on)
 {
-    //ALOGD("%s: %s input devices", __func__, on ? "enabling" : "disabling");
-    write_int(TSP_POWER, on ? 1 : 0);
+    ALOGE("%s: %s input devices", __func__, on ? "enabling" : "disabling");
+
+    if (on)
+        write_int(TSP_POWER, 1);
+    else if (on == 0 && wake_gesture_enabled == 0)
+        write_int(TSP_POWER, 0);
+
+    if (wake_gesture_enabled) {
+      if (on)
+        write_int(TSP_WAKE_GESTURE, 0);
+      else
+        write_int(TSP_WAKE_GESTURE, 1);
+    }
+    
+    
+    if (on) {
+        write_int(GKP_POWER, 1);
+        write_int(TKP_POWER, 1);
+    }
+    else {
+        //write_int(GKP_POWER, 0); /*Do not power down as home key is wakeupsignal*/
+        write_int(TKP_POWER, 0);
+    }
 }
 
 static void power_hint(struct power_module *module, power_hint_t hint,
                        void *data) {
     switch (hint) {
-    default:
-        break;
+        case POWER_HINT_VSYNC:
+            //ALOGE("%s: vsync", __func__);
+            break;
+        case POWER_HINT_INTERACTION:
+            //ALOGE("%s: interaction", __func__);
+            break;
+        case POWER_HINT_LOW_POWER:
+            //ALOGE("%s: low power", __func__);
+            break;
+        default:
+            break;
+    }
+}
+
+static void power_set_feature(struct power_module *module, feature_t feature, int state)
+{
+    switch (feature) {
+        case POWER_FEATURE_DOUBLE_TAP_TO_WAKE:
+            ALOGE("%s: %s double tap to wake", __func__, state ? "enabling" : "disabling");
+            wake_gesture_enabled = state>0?1:0;
+            break;
+        default:
+            break;
     }
 }
 
@@ -77,16 +126,17 @@ static struct hw_module_methods_t power_module_methods = {
 
 struct power_module HAL_MODULE_INFO_SYM = {
     .common = {
-        .tag = HARDWARE_MODULE_TAG,
-        .module_api_version = POWER_MODULE_API_VERSION_0_2,
-        .hal_api_version = HARDWARE_HAL_API_VERSION,
-        .id = POWER_HARDWARE_MODULE_ID,
-        .name = "Chagall Power HAL",
-        .author = "The CyanogenMod Project",
-        .methods = &power_module_methods,
+        .tag                = HARDWARE_MODULE_TAG,
+        .module_api_version = POWER_MODULE_API_VERSION_0_3,
+        .hal_api_version    = HARDWARE_HAL_API_VERSION,
+        .id                 = POWER_HARDWARE_MODULE_ID,
+        .name               = "Chagall Power HAL",
+        .author             = "The CyanogenMod Project",
+        .methods            = &power_module_methods,
     },
 
     .init = power_init,
     .setInteractive = power_set_interactive,
     .powerHint = power_hint,
+    .setFeature = power_set_feature,
 };
